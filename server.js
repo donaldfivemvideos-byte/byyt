ab8a87203feb11252b62b4b1d77a5d0d0ba8b2c3
kinghotice01-wq <kinghotice01@gmail.com>
2026-04-08 14:18:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const allowedOrigins = [
   'http://127.0.0.1:5000',
   'http://localhost:5173/',
   'https://bellskeet.netlify.app',
-  'https://porost.netlify.app'
+  'https://porost.netlify.app',
+  'https://bellcabiz.vercel.app'
    // add all your trusted frontends here
 ];
 
