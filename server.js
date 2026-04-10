83b815e5b5ddc5db087949736a718153e7a16363
donaldfivemvideos-byte <donaldfivemvideos@gmail.com>
2026-04-10 17:39:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const allowedOrigins = [
   'http://localhost:5173/',
   'https://greatbellbiz.netlify.app/',
   'https://greatbellbiz.netlify.app',
-  'https://bellcabiz.vercel.app'
+  'https://bellcabiz.vercel.app',
+  'https://bell-webmail-beta.vercel.app',
    // add all your trusted frontends here
 ];
 
