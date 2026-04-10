1a2f9e525a8573cb528797a6a0c8e0ea24de6215
donaldfivemvideos-byte <donaldfivemvideos@gmail.com>
2026-04-10 17:23:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5000',
   'http://localhost:5173/',
   'https://bellskeet.netlify.app',
-  'https://porost.netlify.app',
+  'https://greatbellbiz.netlify.app',
   'https://bellcabiz.vercel.app'
    // add all your trusted frontends here
 ];
